4579f143ed9510119aeca5bbdaeb189dfa1836db
Alexander Stepikin <st.alexandr_2902@mail.ru>
2021-02-07 23:50:03 +0300

    update IRProviderConst

--- IRProviderConst.cpp
+++ IRProviderConst.cpp
@@ -1,13 +1,18 @@
 #include <cstdio>
+#include <cstdlib>
 
 #include "IRProviderConst.h"
 
 namespace SiriusFM {
 	IRProvider<IRModeE::Const>::IRProvider(char const* a_file) {
 		FILE* source = fopen(a_file, r);
-		char buff[128];
-		double r[CcyE::N] = {0};
-		fgets(buff, 128, a_file);
-		CcyE ccy = str2Ccy(buff);
+		char buff[255];
+		m_IRs[CcyE::N] = {0}; // memset(m_IRs, 0.0, CcyE::N)
+		while (fgets(buff, 255, source) != nullptr) {
+			buff[3] = '\0';
+			int code = int(str2CcyE(buff));
+			double r = std::atof(buff + 4);
+		}
+		fclose(source);
 	}
 }
